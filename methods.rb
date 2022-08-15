0cd4fc69fe48fe26b93e50970ad29e2396d5b879
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-15 12:36:16 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+    
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet ('Naureen')
+
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+greet_with_default("Sunny")
+
+
+
+def add num1, num2
+    puts num1 + num2
+end
+puts add(1, 2)
+
+
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    return number / 2
+end
+puts halve(1)
+
+
